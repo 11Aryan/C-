bad4b16fba1a4573bf4a2c363bf2d6d3370f427a
11Aryan <90818029+11Aryan@users.noreply.github.com>
2022-09-12 06:49:32 +0530

    Add files via upload

---
+++ 01string_para.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char ch[25];
+    string s1;
+    cout<<"Enter :";
+    gets(ch);
+    //int a= strlen(ch);
+    //cout<<a;
+    //puts(ch);
+    //cin.getline(ch,10);
+    cout<<ch;
+    //cin>>s1;
+    //cout<<s1;
+}
